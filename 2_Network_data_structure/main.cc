b559212494565bea9d2fb4f99b4e3cbd5f8fd628
Wenji <keenite@gmail.com>
2018-02-09 15:42:15 -0800

    Example for issue 2
    Signed-off-by: Wenji <keenite@gmail.com>

--- 2_Network_data_structure/main.cc
+++ 2_Network_data_structure/main.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
-/*************************************
-* struct sockaddr {
+/************************************* 
+ * struct sockaddr {
 *         unsigned short sa_family; // address family, AF_xxx
 *             char sa_data[14]; // 14 bytes of protocol address
 * };
@@ -22,17 +23,26 @@ using namespace std;
 * };
 *
 * The sockaddr_in and sockaddr have the same size, we can freely cast from on to another
+* arpa/inet.h
+* int inet_pton(int af, const char *src, void *dst); IPv4 and IPv6 from text to binary form
+* const char *inet_ntop(int af, const void *src, char *dst, socklen_t size); From binary to text
 **************************************/
 
 int main()
 {
-    struct sockaddr_int myaddr;
+    struct sockaddr_in myaddr;
     int s;
+    char *ipaddr_str;
+    unsigned char buf[sizeof(struct in6_addr)];
+    char str[INET6_ADDRSTRLEN];
 
     myaddr.sin_family = AF_INET;
     myaddr.sin_port = htons(3490);
 
-    inet_aton("63.161.169.137", &myaddr.sin_addr.s_addr);
+    inet_aton("63.161.169.137", &myaddr.sin_addr);
+    cout << myaddr.sin_addr.s_addr << "\n";
+    inet_ntop(AF_INET, &myaddr.sin_addr.s_addr, str, INET6_ADDRSTRLEN);
+    cout << str << "\n";
 
     return 0;
 }
